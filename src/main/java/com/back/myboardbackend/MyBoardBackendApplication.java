d2ba2aca0374f9cad0a5412b84e4cd9328af6317
kmindev <kangkm0324@gmail.com>
2024-11-25 23:15:35 +0900

    #4 - 스프링부트 프로젝트 생성
    * 현재 스프링부트 최신 버전은 v3.4이므로, 가장 최신 버전으로 프로젝트를 생성함.
    * 필요해보이는 의존성들을 미리 추가함.(web, security, jpa,  lombok, h2database)

---
+++ src/main/java/com/back/myboardbackend/MyBoardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.back.myboardbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBoardBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBoardBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/back/myboardbackend/MyBoardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.myboardbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBoardBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

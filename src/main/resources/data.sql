d25e6a668ac54556c627a7a1737c2bad4646e916
kmindev <kangkm0324@gmail.com>
2024-12-15 23:12:18 +0900

    #5 - 유저 MOCK 데이터 추가
    초기 개발 단계에서 테스트를 위한 데이터 셋업

---
+++ src/main/resources/data.sql
@@ -0,0 +1,43 @@
+insert into user_account (user_id, user_password, email, nickname, memo, social_provider, social_id, role, created_at,
+                          created_by, modified_at, modified_by)
+values ('admin1', '{noop}qwer1234', 'admin1@naver.com', '별빛1', null, 'kakao', 'qdwd12ddd31dwd',
+        'ROLE_ADMIN', '2015-03-17 23:20:26', 'admin1', '2010-03-05 08:28:54', 'admin1');
+insert into user_account (user_id, user_password, email, nickname, memo, social_provider, social_id, role, created_at,
+                          created_by, modified_at, modified_by)
+values ('user1', '{noop}qwer1234', 'user1@google.com', '아기상어5', null, null, null, 'ROLE_USER',
+        '2002-11-16 04:23:32', 'user1', '2002-11-23 04:51:37', 'user1');
+insert into user_account (user_id, user_password, email, nickname, memo, social_provider, social_id, role, created_at,
+                          created_by, modified_at, modified_by)
+values ('player789', '{noop}aOs420y6g', 'player789@google.com', '별빛2', null, null, null, 'ROLE_USER',
+        '2013-04-04 05:12:41',
+        'player789', '2003-09-26 15:31:56', 'player789');
+insert into user_account (user_id, user_password, email, nickname, memo, social_provider, social_id, role, created_at,
+                          created_by, modified_at, modified_by)
+values ('customer331', '{noop}1L1LNKfD3pvi', 'customer331@daum.com', '아기상어3', null, 'kakao', 'asdqqwddwd12dvsd',
+        'ROLE_USER',
+        '2009-01-27 20:24:30', 'customer331', '2016-12-13 12:27:53', 'customer331');
+insert into user_account (user_id, user_password, email, nickname, memo, social_provider, social_id, role, created_at,
+                          created_by, modified_at, modified_by)
+values ('customer341', '{noop}3Hp271Zo', 'customer341@google.com', '야구팬4', null, null, null, 'ROLE_USER',
+        '2018-10-15 01:02:14', 'customer341', '2006-03-20 14:23:55', 'customer341');
+insert into user_account (user_id, user_password, email, nickname, memo, social_provider, social_id, role, created_at,
+                          created_by, modified_at, modified_by)
+values ('user1234', '{noop}rTw80xiNO2as', 'user1234@google.com', '야구팬2', null, null, null, 'ROLE_USER',
+        '2009-08-14 03:31:32',
+        'user1234', '2007-01-18 11:24:26', 'user1234');
+insert into user_account (user_id, user_password, email, nickname, memo, social_provider, social_id, role, created_at,
+                          created_by, modified_at, modified_by)
+values ('account4256', '{noop}0C1SoS29gaD', 'account4256@google.com', '아기상어1', null, 'kakao', 'adw2d12df3vv', 'ROLE_USER',
+        '2009-07-16 17:59:25', 'account4256', '2013-07-06 03:53:35', 'account4256');
+insert into user_account (user_id, user_password, email, nickname, memo, social_provider, social_id, role, created_at,
+                          created_by, modified_at, modified_by)
+values ('customer3121', '{noop}Bp020cIHj', 'customer3121@naver.com', '달님2', null, null, null, 'ROLE_USER',
+        '2006-04-21 02:25:32', 'customer3121', '2005-12-09 05:51:55', 'customer3121');
+insert into user_account (user_id, user_password, email, nickname, memo, social_provider, social_id, role, created_at,
+                          created_by, modified_at, modified_by)
+values ('player7849', '{noop}7ov0AWw8F24s', 'player7849@naver.com', '별빛4', null, null, null, 'ROLE_USER',
+        '2018-06-07 13:31:11', 'player7849', '2007-06-06 10:23:12', 'player7849');
+insert into user_account (user_id, user_password, email, nickname, memo, social_provider, social_id, role, created_at,
+                          created_by, modified_at, modified_by)
+values ('account4526', '{noop}ukjt0YMye', 'account4526@naver.com', '아기상어2', null, null, null, 'ROLE_USER',
+        '2015-05-05 21:44:54', 'account4526', '2018-01-30 14:20:46', 'account4526');

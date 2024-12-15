fb79544ec60e02288a6365488d1e5a38bb927683
kmindev <kangkm0324@gmail.com>
2024-12-15 23:16:14 +0900

    #5 - 인증 구현에 따른 'JPA AuditorAware' 구현
    'SecurityContext'에 유저 정보를 사용하도록 구현.

--- src/main/java/com/back/config/JpaConfig.java
+++ src/main/java/com/back/config/JpaConfig.java
@@ -1,18 +1,28 @@
 package com.back.config;
 
+import com.back.secuirty.BoardUserDetails;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
 
+@EnableJpaAuditing
 @Configuration
 public class JpaConfig {
 
     @Bean
     public AuditorAware<String> auditorAware() {
-        // TODO: 인증 구현 후 세부 로직 작성
-        return () -> Optional.of("kkm");
+        return () -> Optional.ofNullable(SecurityContextHolder.getContext())
+                .map(SecurityContext::getAuthentication)
+                .filter(Authentication::isAuthenticated)
+                .map(Authentication::getPrincipal)
+                .map(BoardUserDetails.class::cast)
+                .map(BoardUserDetails::getUsername);
     }
 
 }

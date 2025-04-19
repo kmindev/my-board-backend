a9440414f0d07f7db991fa77b5ffa0ae7bc243f3
kmindev <kangkm0324@gmail.com>
2025-04-19 17:30:45 +0900

    #37 - 컨트롤러 요청/응답 로그 기록
    모든 컨트롤러는 `BaseController`를 상속받고, `BaseController` 에서 제공하는 `requestLog()`,`responseLog()` 메서드로 로그를 남기도록 한다.

--- src/main/java/com/back/controler/ArticleController.java
+++ src/main/java/com/back/controler/ArticleController.java
@@ -9,8 +9,10 @@ import com.back.controler.dto.request.NewArticleRequest;
 import com.back.domain.constant.SearchType;
 import com.back.secuirty.BoardUserDetails;
 import com.back.service.ArticleService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -19,72 +21,87 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/v1/articles")
 @RestController
-public class ArticleController {
+public class ArticleController extends BaseController {
 
     private final ArticleService articleService;
 
     @PostMapping
     public ResponseEntity<ApiResponse<ArticleWithHashtagsResponse>> newArticle(
             @RequestBody @Valid NewArticleRequest request,
-            @AuthenticationPrincipal BoardUserDetails boardUserDetails
+            @AuthenticationPrincipal BoardUserDetails boardUserDetails,
+            HttpServletRequest httpServletRequest
     ) {
-        return ResponseEntity.ok().body(
-                ApiResponse.okWithData(
-                        ArticleWithHashtagsResponse.from(
-                                articleService.newArticle(request.toDto(boardUserDetails.toDto()))
-                        )
+        requestLog(log, httpServletRequest, request);
+        ApiResponse<ArticleWithHashtagsResponse> response = ApiResponse.okWithData(
+                ArticleWithHashtagsResponse.from(
+                        articleService.newArticle(request.toDto(boardUserDetails.toDto()))
                 )
         );
+        responseLog(log, httpServletRequest, response);
+        return ResponseEntity.ok().body(response);
     }
 
     @GetMapping
     public ResponseEntity<ApiResponse<Page<SearchArticleResponse>>> getArticles(
             @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
             @RequestParam(required = false) String searchValue,
-            @RequestParam(required = false) SearchType searchType
+            @RequestParam(required = false) SearchType searchType,
+            HttpServletRequest httpServletRequest
     ) {
-        return ResponseEntity.ok().body(
-                ApiResponse.okWithData(
-                        articleService.searchArticles(pageable, searchValue, searchType)
-                                .map(SearchArticleResponse::from)
-                )
+        requestLog(log, httpServletRequest);
+        ApiResponse<Page<SearchArticleResponse>> response = ApiResponse.okWithData(
+                articleService.searchArticles(pageable, searchValue, searchType)
+                        .map(SearchArticleResponse::from)
         );
+        responseLog(log, httpServletRequest, response);
+        return ResponseEntity.ok().body(response);
     }
 
     @GetMapping("/{articleId}")
-    public ResponseEntity<ApiResponse<ArticleDetailsResponse>> getArticleDetails(@PathVariable Long articleId) {
-        return ResponseEntity.ok().body(
-                ApiResponse.okWithData(
-                        ArticleDetailsResponse.from(articleService.getArticleDetails(articleId))
-                )
+    public ResponseEntity<ApiResponse<ArticleDetailsResponse>> getArticleDetails(
+            @PathVariable Long articleId,
+            HttpServletRequest httpServletRequest
+    ) {
+        requestLog(log, httpServletRequest);
+        ApiResponse<ArticleDetailsResponse> response = ApiResponse.okWithData(
+                ArticleDetailsResponse.from(articleService.getArticleDetails(articleId))
         );
+        responseLog(log, httpServletRequest, response);
+        return ResponseEntity.ok().body(response);
     }
 
     @PatchMapping("/{articleId}")
     public ResponseEntity<ApiResponse<ArticleWithHashtagsResponse>> updateArticle(
             @PathVariable Long articleId,
             @RequestBody @Valid ArticleUpdateRequest request,
-            @AuthenticationPrincipal BoardUserDetails boardUserDetails
+            @AuthenticationPrincipal BoardUserDetails boardUserDetails,
+            HttpServletRequest httpServletRequest
     ) {
-        return ResponseEntity.ok().body(
-                ApiResponse.okWithData(
-                        ArticleWithHashtagsResponse.from(
-                                articleService.updateArticle(request.toDto(articleId, boardUserDetails.toDto()))
-                        )
+        requestLog(log, httpServletRequest, request);
+        ApiResponse<ArticleWithHashtagsResponse> response = ApiResponse.okWithData(
+                ArticleWithHashtagsResponse.from(
+                        articleService.updateArticle(request.toDto(articleId, boardUserDetails.toDto()))
                 )
         );
+        responseLog(log, httpServletRequest, response);
+        return ResponseEntity.ok().body(response);
     }
 
     @DeleteMapping("/{articleId}")
     public ResponseEntity<ApiResponse<Void>> updateArticle(
             @PathVariable Long articleId,
-            @AuthenticationPrincipal BoardUserDetails boardUserDetails
+            @AuthenticationPrincipal BoardUserDetails boardUserDetails,
+            HttpServletRequest httpServletRequest
     ) {
+        requestLog(log, httpServletRequest);
         articleService.deleteArticle(articleId, boardUserDetails.userId());
-        return ResponseEntity.ok().body(ApiResponse.okWithMessage("삭제 성공."));
+        ApiResponse<Void> response = ApiResponse.okWithMessage("삭제 성공.");
+        responseLog(log, httpServletRequest, response);
+        return ResponseEntity.ok().body(response);
     }
 
 }

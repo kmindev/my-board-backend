c1d0894272ec9d4ce426fe20652058b9d08f6251
Kang KyeongMin <kangkm0324@gmail.com>
2025-06-15 22:20:32 +0900

    #37 - 컨트롤러 요청/응답 로그 기록 (#39)

--- src/main/java/com/back/controler/CommentController.java
+++ src/main/java/com/back/controler/CommentController.java
@@ -5,39 +5,49 @@ import com.back.controler.dto.reponse.ArticleDetailsResponse;
 import com.back.controler.dto.request.NewCommentRequest;
 import com.back.secuirty.BoardUserDetails;
 import com.back.service.CommentService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/v1/comments")
 @RestController
-public class CommentController {
+public class CommentController extends BaseController {
 
     private final CommentService commentService;
 
     @PostMapping
     public ResponseEntity<ApiResponse<ArticleDetailsResponse>> newComment(
             @RequestBody @Valid NewCommentRequest request,
-            @AuthenticationPrincipal BoardUserDetails boardUserDetails
+            @AuthenticationPrincipal BoardUserDetails boardUserDetails,
+            HttpServletRequest httpServletRequest
     ) {
-        return ResponseEntity.ok().body(
-                ApiResponse.okWithData(
-                        ArticleDetailsResponse.from(
-                                commentService.newComment(request.toDto(boardUserDetails.toDto()))
-                        )
+        requestLog(log, httpServletRequest, request);
+        ApiResponse<ArticleDetailsResponse> response = ApiResponse.okWithData(
+                ArticleDetailsResponse.from(
+                        commentService.newComment(request.toDto(boardUserDetails.toDto()))
                 )
         );
+        responseLog(log, httpServletRequest, response);
+        return ResponseEntity.ok().body(response);
     }
 
     @DeleteMapping("/{commentId}")
     public ResponseEntity<ApiResponse<Void>> deleteComment(
             @PathVariable Long commentId,
-            @AuthenticationPrincipal BoardUserDetails boardUserDetails
+            @AuthenticationPrincipal BoardUserDetails boardUserDetails,
+            HttpServletRequest httpServletRequest
     ) {
+        requestLog(log, httpServletRequest);
         commentService.deleteComment(commentId, boardUserDetails.userId());
-        return ResponseEntity.ok().body(ApiResponse.okWithMessage("삭제 성공."));
+        ApiResponse<Void> response = ApiResponse.okWithMessage("삭제 성공.");
+        responseLog(log, httpServletRequest, response);
+        return ResponseEntity.ok().body(response);
     }
+
 }

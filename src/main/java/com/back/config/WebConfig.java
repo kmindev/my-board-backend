22564f53495b3b87b5876bb6dbe594faffc1a2ea
Kang KyeongMin <kangkm0324@gmail.com>
2025-07-01 21:10:07 +0900

    [Feature] API 요청 처리 시간 로깅 (#41)
    * API 처리 시간 로깅
    * QueryDSL QClass 추적 X

--- src/main/java/com/back/config/WebConfig.java
+++ src/main/java/com/back/config/WebConfig.java
@@ -1,9 +1,11 @@
 package com.back.config;
 
+import com.back.config.interceptor.RequestTimeInterceptor;
 import com.back.controler.converter.SearchTypeRequestConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
@@ -13,6 +15,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new SearchTypeRequestConverter());
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RequestTimeInterceptor());
     }
 
 }

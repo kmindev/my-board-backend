3801a8258f60153610e5d4f0ad1bb7cec27300bf
kmindev <kangkm0324@gmail.com>
2025-07-10 19:21:59 +0900

    #43 - 분리된 시큐리티 설정으로 테스트 환경 구성
    - 테스트에서 실제 OAuth2 설정(Kakao 등) 제거
    - `TestSecurityConfig`를 통해 보안 필터 체인 단순화

--- src/main/java/com/back/config/SecurityConfig.java
+++ src/main/java/com/back/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import com.back.secuirty.general.ApiAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -25,6 +26,7 @@ import org.springframework.security.web.context.HttpSessionSecurityContextReposi
 import org.springframework.security.web.context.RequestAttributeSecurityContextRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+@Profile("!test")
 @RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
---
+++ src/test/java/com/back/config/TestSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.back.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@TestConfiguration
+public class TestSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+}

c3db3091f3f9e3d1a6491fce17ee6a7bce631498
kmindev <kangkm0324@gmail.com>
2025-07-22 16:29:28 +0900

    #47 - 게시글 검색 타입 컨버터 제거
    기존에는 enum의 한글명(typeName)을 기준으로 요청에 매핑하기 위해 커스텀 컨버터를 사용했었다.
    하지만, 클라이언트에서 요청 시 enum 상수명(TITLE, HASHTAG 등) 을 그대로 사용할 수 있도록 컨버터를 제거함.

--- src/main/java/com/back/config/WebConfig.java
+++ src/main/java/com/back/config/WebConfig.java
@@ -1,21 +1,14 @@
 package com.back.config;
 
 import com.back.config.interceptor.RequestTimeInterceptor;
-import com.back.controler.converter.SearchTypeRequestConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
-import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new SearchTypeRequestConverter());
-    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {

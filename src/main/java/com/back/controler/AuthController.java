039c83dae00b6a815e2c072b040f12d7d33cd000
kmindev <kangkm0324@gmail.com>
2025-07-23 18:00:05 +0900

    #47 - 스웨거 어노테이션을 통한 스웨거 문서 적용

--- src/main/java/com/back/controler/AuthController.java
+++ src/main/java/com/back/controler/AuthController.java
@@ -2,6 +2,8 @@ package com.back.controler;
 
 import com.back.controler.dto.reponse.ApiResponse;
 import com.back.secuirty.BoardUserDetails;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -9,19 +11,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "인증 API")
 @RequiredArgsConstructor
 @RequestMapping("/v1/auth")
 @RestController
 public class AuthController {
 
+    @Operation(summary = "관리자 권한 확인 API")
     @GetMapping("/admin-test")
     public ResponseEntity<ApiResponse<String>> adminTest(@AuthenticationPrincipal BoardUserDetails boardUserDetails) {
-        return ResponseEntity.ok().body(ApiResponse.okWithMessageAndData("admin test api", "hello: " + boardUserDetails));
+        return ResponseEntity.ok()
+                .body(ApiResponse.okWithMessageAndData("admin test api", "hello: " + boardUserDetails));
     }
 
+    @Operation(summary = "사용자 권한 확인 API")
     @GetMapping("/user-test")
     public ResponseEntity<ApiResponse<String>> userTest(@AuthenticationPrincipal BoardUserDetails boardUserDetails) {
-        return ResponseEntity.ok().body(ApiResponse.okWithMessageAndData("user test api", "hello: " + boardUserDetails));
+        return ResponseEntity.ok()
+                .body(ApiResponse.okWithMessageAndData("user test api", "hello: " + boardUserDetails));
     }
 
 }

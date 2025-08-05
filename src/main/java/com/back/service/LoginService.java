2535ddc4410899cb3318df26d034b34700f0e40f
kmindev <kangkm0324@gmail.com>
2025-08-05 17:14:04 +0900

    [Feat] Authorites는 UserDetails 구현체에 정의된 메서드로 수정

--- src/main/java/com/back/service/LoginService.java
+++ src/main/java/com/back/service/LoginService.java
@@ -4,9 +4,7 @@ import com.back.secuirty.BoardUserDetails;
 import com.back.service.dto.UserAccountDto;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
-import java.util.List;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
@@ -14,10 +12,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class LoginService {
+
     public void login(UserAccountDto userAccountDto, HttpServletRequest request) {
         BoardUserDetails boardUserDetails = BoardUserDetails.from(userAccountDto);
         UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
-                boardUserDetails, null, List.of(new SimpleGrantedAuthority(userAccountDto.role().getName())));
+                boardUserDetails, null, boardUserDetails.getAuthorities());
 
         SecurityContext context = SecurityContextHolder.createEmptyContext();
         context.setAuthentication(auth);
@@ -26,4 +25,5 @@ public class LoginService {
         HttpSession session = request.getSession(true);
         session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, context);
     }
+
 }

8713ffd7ce59c6b1c9b1977f7d2a51c2793daaa2
kmindev <kangkm0324@gmail.com>
2025-08-12 21:38:14 +0900

    [Test] MockMvc 인증용 BoardUserDetails RequestPostProcessor 유틸 클래스 추가
    - with(user("user1).roles("USER")) 방식 대신, 컨트롤러에서 사용하는 BoardUserDetails 객체를 직접 주입할 수 있도록 구현
    - principal 객체 타입 불일치 문제 해결

---
+++ src/test/java/com/back/config/TestSecurityUtil.java
@@ -0,0 +1,24 @@
+package com.back.config;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
+
+import com.back.domain.UserRoleType;
+import com.back.secuirty.BoardUserDetails;
+import java.util.List;
+import java.util.Map;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.test.web.servlet.request.RequestPostProcessor;
+
+public class TestSecurityUtil {
+
+    public static RequestPostProcessor boardUserDetails(String username, UserRoleType roleType) {
+        BoardUserDetails boardUserDetails = new BoardUserDetails(
+                username, "1234", null, null, null,
+                null, null, roleType, List.of(new SimpleGrantedAuthority(roleType.getName())), Map.of()
+        );
+        return authentication(new UsernamePasswordAuthenticationToken(boardUserDetails, boardUserDetails.getPassword(),
+                boardUserDetails.getAuthorities()));
+    }
+
+}
